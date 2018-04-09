b11549ec10b5f59fb83036dcf9b2129bbc8d6b44
aeh-bonilla <aeh.bonilla@gmail.com>
2018-04-09 01:10:20 -0500

    se re nombra el metodo init_api por init tasks

--- rocket/urls.py
+++ rocket/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
-from .views import say_hi, init_api, add_task, update_task, delete_task, list_tasks_by_description, list_task_by_status
+from .views import say_hi, init_tasks, add_task, update_task, delete_task, list_tasks_by_description, list_task_by_status
 
 app_name = 'rocket'
 
 urlpatterns = [
     url(r'^api/hi/', say_hi, name='say_hi'),
-    url(r'^api/init_api/', init_api, name='init_api'),
+    url(r'^api/init_tasks/', init_tasks, name='init_tasks'),
     url(r'^api/search/(?P<q>[\w-]+)/$', list_tasks_by_description, name='list_tasks_by_description'),
     url(r'^api/search_status/(?P<q>[\w-]+)/$', list_task_by_status, name='list_tasks_by_status'),
     url(r'^api/add_task/', add_task, name='add_task'),
