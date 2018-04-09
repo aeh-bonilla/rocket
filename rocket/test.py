01b50566756baa951dcccb95d1cb32bc5212e02d
aeh-bonilla <aeh.bonilla@gmail.com>
2018-04-09 01:14:36 -0500

    se re nombra metodo

---
+++ rocket/test.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+
+class RocketTest(APITestCase):
+
+    def test_add_task(self):
+
+        client = APIClient()
+        response=client.post('/api/add_task/', {'description': 'test 2',
+                'duration':'1'})
+
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_task(self):
+        client = APIClient()
+        response=client.post('/api/delete_task/', {'id': '7'})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
